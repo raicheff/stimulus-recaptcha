d1408a2f727b786fbe4f7dd90b9cd6e838d5ce42
Boris Raicheff <b@raicheff.com>
2019-03-09 17:57:52 +0000

    Hello, world!

---
+++ stimulus-recaptcha.js
@@ -0,0 +1,33 @@
+/*
+ * Stimulus-reCAPTCHA
+ *
+ * Copyright (C) 2019 Boris Raicheff
+ * All rights reserved
+ */
+
+
+import { Controller } from "stimulus";
+
+
+export default class extends Controller {
+
+  static targets = ["token"];
+
+  connect() {
+
+    const recaptcha = window.grecaptcha;
+
+    // https://developers.google.com/recaptcha/docs/v3
+
+    recaptcha.ready(() => {
+      recaptcha.execute(this.data.get("site-key"), { action: this.data.get("action") }).then(token => {
+        this.tokenTarget.value = token;
+      });
+    });
+
+  }
+
+}
+
+
+/* EOF */
